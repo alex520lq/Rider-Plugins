d2568b59cc9b07a00d97afcf591a625406cfca09
GitHub Action <action@github.com>
2024-09-27 03:36:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/alex520lq/riderplugins/MyBundle.kt
+++ src/main/kotlin/com/github/alex520lq/riderplugins/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.alex520lq.riderplugins
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/alex520lq/riderplugins/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/alex520lq/riderplugins/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.alex520lq.riderplugins.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
